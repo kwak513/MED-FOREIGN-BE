745125f2760af4ff2adf46f5501c2e0d8be78e77
kwak513 <kwak513@gmail.com>
2025-04-21 23:28:57 +0900

    병원 상세페이지에서 필요한 gangnam, gangdong hospital 정보 SELECT

--- src/main/java/com/medical/controller/HospitalController.java
+++ src/main/java/com/medical/controller/HospitalController.java
@@ -17,7 +17,7 @@ public class HospitalController {
 	@Autowired
 	HospitalService hospitalService;
 
-	// 강남구와 강동구 병원 정보 가져오기(병원명, 시·구 주소, 가능 언어, 대표과 1개), 메인 리스트 페이지에서 사용할거임.
+	// 메인 리스트 페이지에서 사용할 강남구와 강동구 병원 정보 가져오기(id, 병원명, 시·구 주소, 가능 언어, 대표과 1개)
 	@GetMapping("/select15FromGangnamGangDongHospital")
 	public List<Map<String, Object>> select15FromGangnamGangDongHospital(@RequestParam int offsetNum){
 		return hospitalService.select15FromGangnamGangDongHospital(offsetNum);
@@ -28,19 +28,59 @@ public class HospitalController {
 	  {
 	    "hospital_languages": "영어",
 	    "source": "gangdong",
-	    "hospital_main_category": "피부과",
+	    "hospital_main_category": "내과",
+	    "hospital_id": 1,
 	    "hospital_main_address": "서울시 강동구",
-	    "hospital_name": "비너스의원"
+	    "hospital_name": "중앙보훈병원"
 	  },
 	  {
-	    "hospital_languages": "영어",
+	    "hospital_languages": "영어,일어",
 	    "source": "gangdong",
-	    "hospital_main_category": "산부인과",
+	    "hospital_main_category": "신경외과",
+	    "hospital_id": 2,
 	    "hospital_main_address": "서울시 강동구",
-	    "hospital_name": "강동미즈여성병원"
+	    "hospital_name": "허리나은병원"
 	  }, {}, {} , ...
 	 ]
 	 */
+	
+	// 병원 상세 페이지에서 사용할 '강남구' 병원 정보 가져오기
+	@GetMapping("/selectFromGangnamHospital")
+	public List<Map<String, Object>> selectFromGangnamHospital(@RequestParam int hospitalId){
+		return hospitalService.selectFromGangnamHospital(hospitalId);
+	}
+	
+	/*
+	[
+	  {
+	    "hospital_category": "의원",
+	    "hospital_languages": "중국",
+	    "hospital_phone_number": "02-6958-7532",
+	    "hospital_main_address": "서울시 강남구",
+	    "hospital_address": "서울특별시 강남구 봉은사로 230 (역삼동, 봉암빌딩) 5층",
+	    "hospital_name": "서울베스트비뇨의학과의원"
+	  }
+	]
+	 */
+	
+	// 병원 상세 페이지에서 사용할 '강동구' 병원 정보 가져오기
+	@GetMapping("/selectFromGangdongHospital")
+	public List<Map<String, Object>> selectFromGangdongHospital(int hospitalId){
+		return hospitalService.selectFromGangdongHospital(hospitalId);
+	}
+	/*
+	[
+	  {
+	    "hospital_category": "신경외과,내과,정형외과,영상의학과,마취통증의학과",
+	    "hospital_languages": "영어,일어",
+	    "hospital_phone_number": "02-472-0114",
+	    "hospital_main_address": "서울시 강동구",
+	    "hospital_address": "서울 강동구 성내2동 62-4",
+	    "hospital_name": "허리나은병원"
+	  }
+	]
+	 */
+	
 	
 
 //	@PostMapping("/insertIntoChartInfo")
--- src/main/java/com/medical/service/HospitalService.java
+++ src/main/java/com/medical/service/HospitalService.java
@@ -20,7 +20,7 @@ public class HospitalService {
 	
 	
 
-	// 강남구와 강동구 병원 정보 가져오기(병원명, 시·구 주소, 가능 언어, 대표과 1개), 메인 리스트 페이지에서 사용할거임.
+	// 메인 리스트 페이지에서 사용할 강남구와 강동구 병원 정보 가져오기(id, 병원명, 시·구 주소, 가능 언어, 대표과 1개)
 	public List<Map<String, Object>> select15FromGangnamGangDongHospital(int offsetNum){
 		
 		try {
@@ -29,7 +29,8 @@ public class HospitalService {
 						+ "gangdong_languages AS hospital_languages, "
 						+ "gangdong_main_address AS hospital_main_address, "
 						+ "SUBSTRING_INDEX(gangdong_category, ',', 1) AS hospital_main_category, "
-						+ "'gangdong' AS source "
+						+ "'gangdong' AS source, "
+						+ "id AS hospital_id "
 						+ "FROM gangdong_hospital "
 						+ "LIMIT 15 OFFSET :OFFSET) "
 						+ "UNION "
@@ -37,7 +38,8 @@ public class HospitalService {
 						+ "gangnam_languages AS hospital_languages, "
 						+ "gangnam_main_address AS hospital_main_address, "
 						+ "SUBSTRING_INDEX(gangnam_category, ',', 1) AS hospital_main_category, "
-						+ "'gangnam' AS source "
+						+ "'gangnam' AS source, "
+						+ "id AS hospital_id "
 						+ "FROM gangnam_hospital "
 						+ "LIMIT 15 OFFSET :OFFSET);"
 					;
@@ -55,6 +57,56 @@ public class HospitalService {
 		}
 	}
 
+	// 병원 상세 페이지에서 사용할 '강남구' 병원 정보 가져오기
+	public List<Map<String, Object>> selectFromGangnamHospital(int hospitalId){
+		
+		try {
+			String sql = "SELECT gangnam_name AS hospital_name, "
+					+ "gangnam_phone_number AS hospital_phone_number, "
+					+ "gangnam_languages AS hospital_languages, "
+					+ "gangnam_main_address AS hospital_main_address, "
+					+ "gangnam_address AS hospital_address, "
+					+ "gangnam_category AS hospital_category "
+					+ "FROM gangnam_hospital WHERE ID = :ID";;
+			Query query = em.createNativeQuery(sql, Tuple.class);
+			query.setParameter("ID", hospitalId);
+		
+			List<Tuple> rs = query.getResultList();
+			
+			List<Map<String, Object>> rsToMap = JPAUtil.convertTupleToMap(rs);
+			return rsToMap;
+			
+		} catch(Exception e) {
+			System.out.println("selectFromGangnamHospital failed: "+ e.getMessage());
+			return new ArrayList<>();
+		}
+	}
+
 	
-	
+	// 병원 상세 페이지에서 사용할 '강동구' 병원 정보 가져오기
+	public List<Map<String, Object>> selectFromGangdongHospital(int hospitalId){
+		
+		try {
+			String sql = "SELECT gangdong_name AS hospital_name, "
+					+ "gangdong_phone_number AS hospital_phone_number, "
+					+ "gangdong_languages AS hospital_languages, "
+					+ "gangdong_main_address AS hospital_main_address, "
+					+ "gangdong_address AS hospital_address, "
+					+ "gangdong_category AS hospital_category "
+					+ "FROM gangdong_hospital WHERE ID = :ID";
+			
+			
+			Query query = em.createNativeQuery(sql, Tuple.class);
+			query.setParameter("ID", hospitalId);
+		
+			List<Tuple> rs = query.getResultList();
+			
+			List<Map<String, Object>> rsToMap = JPAUtil.convertTupleToMap(rs);
+			return rsToMap;
+			
+		} catch(Exception e) {
+			System.out.println("selectFromGangdongHospital failed: "+ e.getMessage());
+			return new ArrayList<>();
+		}
+	}
 }
